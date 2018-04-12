9b75771247062b361bf90d6788593add0786bba0
Jonas Hartmann <jonas.hartmann@ymail.com>
2018-04-12 16:57:54 +0200

    #4 Lifecycle: Adds start screen that loads default scene after user input

--- Assets/Source/UI/Components/StartScreenManager.cs
+++ Assets/Source/UI/Components/StartScreenManager.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScreenManager : MonoBehaviour {
     
-	public void startGame()
+	public void StartGame()
     {
-           
+        SceneManager.LoadSceneAsync("Default");
     }
 }
