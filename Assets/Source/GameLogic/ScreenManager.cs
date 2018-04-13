ff467fbff0f1a0000b12c4eba7b00d3528113c5b
Jonas Hartmann <jonas.hartmann@ymail.com>
2018-04-13 11:02:14 +0200

    Refactors StartScreenManager into ScreenManager
    The plan is to use it for handling the flow of scenes
    Relates to #5 and #6

--- Assets/Source/GameLogic/ScreenManager.cs
+++ Assets/Source/GameLogic/ScreenManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartScreenManager : MonoBehaviour {
+public class ScreenManager : MonoBehaviour {
     
 	public void StartGame()
     {
