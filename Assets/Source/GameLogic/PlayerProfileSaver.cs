923d81081e88d5c78164afa2f4e4dc05d4431345
Jonas Hartmann <jonas.hartmann@ymail.com>
2018-04-27 11:24:56 +0200

    Ensures that player profile data is stored to disk before loading next scene

---
+++ Assets/Source/GameLogic/PlayerProfileSaver.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Source.GameLogic
+{
+    class PlayerProfileSaver: MonoBehaviour
+    {
+        private void Awake()
+        {
+            // Ensures that data is saved to disk before loading next scene
+            App.Cache.screenManager.OnStartLoading(PlayerPrefs.Save);
+        }
+    }
+}
