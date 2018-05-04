d7b6461ac79a71170386b3d6d6c41df2871359cc
Sebastian Maleck <s.maleck@outlook.de>
2018-05-04 12:46:39 +0200

    Adds Pulsating Button to start screen

---
+++ Assets/Editor/CustomEditors/PulsatingButtonEditor.cs
@@ -0,0 +1,10 @@
+﻿using Assets.Source.UI.Elements;
+using UnityEditor;
+
+namespace Assets.Editor.CustomEditors
+{
+    [CustomEditor(typeof(PulsatingButton))]
+    public class PulsatingButtonEditor : UnityEditor.Editor
+    {
+    }
+}
---
+++ Assets/Source/UI/Panels/TitleMenuPanel.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Source.UI.Panels
+{
+    public class TitleMenuPanel : MonoBehaviour
+    {
+        [SerializeField] Button startButton;
+
+        private void Start()
+        {
+            startButton.OnClickAsObservable().Subscribe(_ => App.Cache.screenManager.ToGame());
+        }
+    }
+}
