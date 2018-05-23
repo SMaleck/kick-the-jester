1e598b865208f34e18bbf1b706cf278f7531a722
Jonas Hartmann <jonas.hartmann@ymail.com>
2018-05-23 16:17:27 +0200

    Adds SpawningLanes config plus custom inspector changes

---
+++ Assets/Editor/Inspector/SpawningLanesInspector.cs
@@ -0,0 +1,108 @@
+﻿using Assets.Source.Config;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System;
+using Assets.Source.Behaviours.Items;
+
+namespace Assets.Editor.Inspector
+{
+    [Flags]
+    public enum EditorListOption {
+        None = 0,
+        ListSize = 1,
+        ListLabel = 2,
+        Buttons = 4,
+        Default = ListLabel,
+        All = Default | Buttons
+    }
+
+    [CustomEditor(typeof(SpawningLanesConfig))]
+    public class SpawningLanesInspector : UnityEditor.Editor
+    {
+        private static GUIContent moveButtonContent = new GUIContent("\u21b4", "move down"),
+                                  duplicateButtonContent = new GUIContent("+", "duplicate"),
+                                  deleteButtonContent = new GUIContent("-", "delete"),
+                                  addButtonContent = new GUIContent("+", "add element");
+        private static GUILayoutOption miniButtonWidth = GUILayout.Width(20f);
+
+        public static void Show(SerializedProperty list, EditorListOption options = EditorListOption.Default)
+        {
+            bool showListLabel = (options & EditorListOption.ListLabel) != 0,
+                 showListSize = (options & EditorListOption.ListSize) != 0;
+
+            if (showListLabel)
+            {
+                EditorGUILayout.PropertyField(list);
+                EditorGUI.indentLevel += 1;
+            }
+            
+            if (!showListLabel || list.isExpanded)
+            {
+                if (showListSize) EditorGUILayout.PropertyField(list.FindPropertyRelative("Array.size"));
+                ShowElements(list, options);
+            }
+            if (showListLabel) EditorGUI.indentLevel -= 1;
+        }
+
+        private static void ShowElements(SerializedProperty list, EditorListOption options)
+        {
+            bool showButtons = (options & EditorListOption.Buttons) != 0;
+
+            for (int i = 0; i < list.arraySize; i++)
+            {
+                if (showButtons) EditorGUILayout.BeginHorizontal();
+
+                SerializedProperty property = list.GetArrayElementAtIndex(i);
+                EditorGUILayout.PropertyField(property, true);
+
+                if (showButtons)
+                {
+                    ShowButtons(list, i);
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+
+            if (showButtons && GUILayout.Button(addButtonContent, EditorStyles.miniButton))
+            {
+                list.arraySize += 1;
+            }
+        }
+
+        private static void ShowButtons(SerializedProperty list, int index)
+        {
+            if (GUILayout.Button(moveButtonContent, EditorStyles.miniButtonLeft, miniButtonWidth))
+            {
+                list.MoveArrayElement(index, index + 1);
+            }
+            if (GUILayout.Button(duplicateButtonContent, EditorStyles.miniButtonMid, miniButtonWidth))
+            {
+                list.InsertArrayElementAtIndex(index);
+            }
+            if (GUILayout.Button(deleteButtonContent, EditorStyles.miniButtonRight, miniButtonWidth))
+            {
+                int oldSize = list.arraySize;
+                list.DeleteArrayElementAtIndex(index);
+                if (list.arraySize == oldSize)
+                {
+                    list.DeleteArrayElementAtIndex(index);
+                }
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("SpawnChance"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("SpawnOnTrajectory"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("ProjectionMaxDeviation"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("MinDistanceBetweenSpawns"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("minimumHeight"));
+            Show(serializedObject.FindProperty("SpawningLanes"), EditorListOption.All);
+
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Source/Behaviours/Items/SpawningLane.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Source.App;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Source.Behaviours.Items
+{
+    [System.Serializable]
+    public class SpawningLane
+    {
+        public float minHeight;
+        public float maxHeight;
+        public List<GameObject> itemPool;
+
+        public GameObject this[int key]
+        {
+            get { return itemPool[key]; }
+            set { itemPool[key] = value; }
+        }
+    }
+}
