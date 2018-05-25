a238605f3e66a17ac8c026c1af4f33c8116a45cf
Sebastian Maleck <s.maleck@outlook.de>
2018-05-25 11:50:01 +0200

    Completes new Parallaxer, Closes #60

--- Assets/Source/Behaviours/Parallax/ParallaxLayer.cs
+++ Assets/Source/Behaviours/Parallax/ParallaxLayer.cs
@@ -11,11 +11,24 @@ namespace Assets.Source.Behaviours.Parallax
 
         private Vector2 lastCameraPos;
         private List<GameObject> Tiles;
-               
+
+        private float spriteWidth;
         private int leftTileIndex;
         private int rightTileIndex;
-        
-        
+
+        private Vector3 leftTilePosition
+        {
+            get { return Tiles[leftTileIndex].transform.position; }
+            set { Tiles[leftTileIndex].transform.position = value; }
+        }
+
+        private Vector3 rightTilePosition
+        {
+            get { return Tiles[rightTileIndex].transform.position; }
+            set { Tiles[rightTileIndex].transform.position = value; }
+        }
+
+
         public ParallaxLayer(Parallaxer owner, ParallaxLayerConfig config, Transform cameraTransform)
             : base (owner, false)
         {
@@ -33,8 +46,9 @@ namespace Assets.Source.Behaviours.Parallax
         {
             Tiles = new List<GameObject>() { CreateTile() };
 
+            // Get Sprite Width
             SpriteRenderer sprite = Tiles.First().GetComponent<SpriteRenderer>();
-            float spriteWidth = sprite.bounds.size.x;
+            this.spriteWidth = sprite.bounds.size.x;
 
             // If this layer should repeat, create 2 adjecent tiles
             if (config.Repeat)
@@ -72,51 +86,86 @@ namespace Assets.Source.Behaviours.Parallax
             MoveTiles();
             lastCameraPos = cameraTransform.position;
 
-            // Switch tiles if camera moved pass
-            //SwitchTiles();            
+            // Switch tiles for infinite background
+            SwitchTiles();            
         }
 
 
         // Moves all tiles to follow the camera according to the parallax speed
         private void MoveTiles()
         {
-            if (config.UseSpeed)
+            if (!config.UseSpeed) { return; }
+
+            foreach (GameObject go in Tiles)
             {
-                foreach(GameObject go in Tiles)
-                {
-                    float deltaX = cameraTransform.position.x - lastCameraPos.x;
-                    go.transform.position += Vector3.right * (deltaX * config.Speed);
-                }
+                float deltaX = cameraTransform.position.x - lastCameraPos.x;
+                go.transform.position += Vector3.right * (deltaX * config.Speed);
             }
         }
 
-        /*
+        
+        // Switches the Tiles so that the Camera always sees the middle one, i.e. inifnite scrolling
         private void SwitchTiles()
         {
-            if (config.Repeat)
+            if (!config.Repeat) { return; }
+
+            if (CameraEnteredLeftTile())
             {
-                if (cameraTransform.position.x >= spriteTransforms[leftSpriteIndex].position.x + spriteWidth)
-                {
-                    ScrollRight();
-                }
+                ScrollLeft();
+            }
+            else if (CameraEnteredRightTile())
+            {
+                ScrollRight();
             }
         }
-        
+
+
+        // Checks if the camera entered the left tile
+        private bool CameraEnteredLeftTile()
+        {
+            return cameraTransform.position.x <= leftTilePosition.x + spriteWidth / 2;
+        }
+
+
+        // Checks if the camera entered the right tile
+        private bool CameraEnteredRightTile()
+        {
+            return cameraTransform.position.x >= rightTilePosition.x - spriteWidth / 2;
+        }
+
 
         // Moves the left-most sprite to the right end
         private void ScrollRight()
         {
-            Vector3 nextPos = new Vector3(spriteTransforms[rightSpriteIndex].position.x + spriteWidth, spriteTransforms[rightSpriteIndex].position.y, spriteTransforms[rightSpriteIndex].position.z);
-            spriteTransforms[leftSpriteIndex].position = nextPos;
+            Vector3 nextPos = new Vector3(rightTilePosition.x + spriteWidth, rightTilePosition.y, rightTilePosition.z);
 
-            rightSpriteIndex = leftSpriteIndex;
-            leftSpriteIndex++;
+            leftTilePosition = nextPos;
 
-            if (leftSpriteIndex >= spriteTransforms.Length)
+            rightTileIndex = leftTileIndex;
+            leftTileIndex++;
+
+            if (leftTileIndex >= Tiles.Count)
             {
-                leftSpriteIndex = 0;
+                leftTileIndex = 0;
             }
         }
-        */
+
+
+        // Moves the right-most sprite to the left end
+        private void ScrollLeft()
+        {
+            Vector3 nextPos = new Vector3(leftTilePosition.x - spriteWidth, leftTilePosition.y, leftTilePosition.z);
+
+            rightTilePosition = nextPos;
+
+            leftTileIndex = rightTileIndex;
+            rightTileIndex--;
+
+            if (rightTileIndex < 0)
+            {
+                rightTileIndex = Tiles.Count -1;
+            }
+        }
+
     }    
 }
