949b4b903235edbee9dd607df9075d73abf7a431
Sebastian Maleck <s.maleck@outlook.de>
2018-05-28 11:28:39 +0200

    Adds more levels for all upgrade paths

--- Assets/Source/App/Upgrade/UpgradeTree.cs
+++ Assets/Source/App/Upgrade/UpgradeTree.cs
@@ -7,18 +7,22 @@ namespace Assets.Source.App.Upgrade
         public static readonly UpgradePath<float> MaxVelocityPath = new UpgradePath<float>(
             new List<UpgradeStep<float>>()
             {
-                new UpgradeStep<float> { Cost = 0, Value = 50f },
-                new UpgradeStep<float> { Cost = 150, Value = 75f },
-                new UpgradeStep<float> { Cost = 200, Value = 100f }
+                new UpgradeStep<float> { Cost = 0, Value = 30f },
+                new UpgradeStep<float> { Cost = 150, Value = 40f },
+                new UpgradeStep<float> { Cost = 300, Value = 50f },
+                new UpgradeStep<float> { Cost = 600, Value = 70f },
+                new UpgradeStep<float> { Cost = 1200, Value = 110f }
             }
         );
 
         public static readonly UpgradePath<float> KickForcePath = new UpgradePath<float>(
             new List<UpgradeStep<float>>()
             {
-                new UpgradeStep<float> { Cost = 0, Value = 17f },
-                new UpgradeStep<float> { Cost = 400, Value = 21f },
-                new UpgradeStep<float> { Cost = 800, Value = 35f }
+                new UpgradeStep<float> { Cost = 0, Value = 13f },
+                new UpgradeStep<float> { Cost = 300, Value = 15f },
+                new UpgradeStep<float> { Cost = 650, Value = 20f },
+                new UpgradeStep<float> { Cost = 1500, Value = 25f },
+                new UpgradeStep<float> { Cost = 3000, Value = 40f }
             }
         );
 
@@ -26,8 +30,10 @@ namespace Assets.Source.App.Upgrade
             new List<UpgradeStep<float>>()
             {
                 new UpgradeStep<float> { Cost = 0, Value = 8f },
-                new UpgradeStep<float> { Cost = 400, Value = 10f },
-                new UpgradeStep<float> { Cost = 800, Value = 12f }
+                new UpgradeStep<float> { Cost = 500, Value = 10f },
+                new UpgradeStep<float> { Cost = 1000, Value = 15f },
+                new UpgradeStep<float> { Cost = 2000, Value = 20f },
+                new UpgradeStep<float> { Cost = 3000, Value = 28f }
             }
         );
 
@@ -35,8 +41,10 @@ namespace Assets.Source.App.Upgrade
             new List<UpgradeStep<int>>()
             {
                 new UpgradeStep<int> { Cost = 0, Value = 3 },
-                new UpgradeStep<int> { Cost = 400, Value = 4 },
-                new UpgradeStep<int> { Cost = 800, Value = 5 }
+                new UpgradeStep<int> { Cost = 200, Value = 4 },
+                new UpgradeStep<int> { Cost = 600, Value = 5 },
+                new UpgradeStep<int> { Cost = 1000, Value = 7 },
+                new UpgradeStep<int> { Cost = 2000, Value = 9 }
             }
         );
     }
